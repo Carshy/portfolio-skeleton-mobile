4427af733ef76fe96f3cdf80331961084e9d3300
Collins <mussocollins@gmail.com>
2022-06-21 13:01:03 +0300

    Added javaScript code to make the nav menu more interactive when the user clicks

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const menuX = document.querySelector("menu-x")
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    menuX.classList.toggle("active");
 })
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
