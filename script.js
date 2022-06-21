e76e0ea95f69ce274b33018f804f565fbe546faa
Collins <mussocollins@gmail.com>
2022-06-21 17:11:53 +0300

    Made changes to style.css to fix linter errors

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const menuX = document.querySelector("menu-x")
+// eslint linebreak-style: ["error", "windows"]
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const menuX = document.querySelector('menu-x');
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-    menuX.classList.toggle("active");
-})
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  menuX.classList.toggle('active');
+});
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
